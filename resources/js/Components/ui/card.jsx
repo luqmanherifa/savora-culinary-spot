c61a6961818e12a76478313dcd1537435e8a21bc
Luqman Herifa <luqmanherifa@gmail.com>
2023-12-06 13:38:26 +0700

    Creating restaurant cards

--- resources/js/Components/ui/card.jsx
+++ resources/js/Components/ui/card.jsx
@@ -26,7 +26,10 @@ CardHeader.displayName = "CardHeader";
 const CardTitle = React.forwardRef(({ className, ...props }, ref) => (
     <h3
         ref={ref}
-        className={cn("text-lg font-bold leading-6 tracking-tight", className)}
+        className={cn(
+            "text-base font-bold leading-5 tracking-tight",
+            className
+        )}
         {...props}
     />
 ));
